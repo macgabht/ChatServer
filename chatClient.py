06353dcb4b5b323cabb4eb912d9844d224473599
macgabht <macgabht@tcd.ie>
2017-11-01 15:23:44 +0000

    WITHOUT HELO, KILL_SERVICE

--- chatClient.py
+++ chatClient.py
@@ -1,8 +1,11 @@
+import sys, socket, select, pdb
 from t_util import Hall, Room, Player
 import t_util
+import time, random
 
-CLIENT_IP = ''
-PORT = ''
+CLIENT_NAME = random.randint(1,50001)
+CLIENT_IP = 0
+PORT = 22222
 RECV_BUFFER = 4096 #make preferably factor of 2
 
 if len(sys.argv) < 2: #i.e [1] = localhost [2] = 5000
@@ -12,7 +15,7 @@ if len(sys.argv) < 2: #i.e [1] = localhost [2] = 5000
 else:
      server_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
      server_connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-     server_connection.connect((sys.argv[1], pychat_util.PORT))  #sys.argv[1] = host,
+     server_connection.connect((sys.argv[1], t_util.PORT))  #sys.argv[1] = host,
 
 def prompt(): #called before each message is written by client
      print '>'
@@ -23,21 +26,60 @@ msg_pref = ''
 socket_list = [sys.stdin, server_connection]
 
 while True:
-	read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
-	for s in read_sockets: 
-		if s is server_connection: #message is being received
-			msg = s.recv(RECV_BUFFER) #assign datat received by buffer to msg	
-			if not msg: #nothing received?
-				print "Something went wrong, server is down!"
-				sys.exit(2)
-			else:
-			    sys.stdout.write(msg) #receives message from server and prints it  to the screen, "Welcome""
-			    if 'Welcome" in msg:
-			    	msg_pref = ''
-			    else:
-				msg_pref = '' #ordinary messages
-			    prompt()
-		else:
-		msg = msg_pref + sys.stdin.readline() #JOIN_CHATROOM .....
-		server_connection.sendall(msg)
+    read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
+    for s in read_sockets:
+        if s is server_connection: # incoming message 
+            msg = s.recv(RECV_BUFFER)
+            if not msg:
+                print("Server down!")
+                sys.exit(2)
+            else:
+                if  msg == t_util.QUIT_STRING:
+                    sys.stdout.write(QUIT_STRING)
+                    sys.exit(2)
+                elif "LEFT_CHATROOM" in msg:
+                    sys.stdout.write(msg)
+                    
+                elif "JOINED_CHATROOM" in msg:
+                    ROOM_REF = msg.split()[7]
+                    JOIN_ID = msg.split()[9]
+                    sys.stdout.write(msg)
+                    msg_prefix = ''
+               
+                else:
+                    sys.stdout.write(msg)
+                    msg_prefix = ''
+                prompt()
+
+        else:
+            msg = msg_prefix + sys.stdin.readline()
+            if "<join>" in msg:
+                room_name = msg.split()[1]
+                msg = ('JOIN_CHATROOM: '+ str(room_name) +'\n'+ 'CLIENT_IP: ' + str(CLIENT_IP)  + '\n'
+                + 'PORT: ' + str(PORT) + '\n' +'CLIENT_NAME: ' + str(CLIENT_NAME)  + '\n')	
+                server_connection.sendall(msg)
+			
+            elif '<leave>' in msg:
+                msg = ('LEAVE_CHATROOM: ' + str(ROOM_REF) + '\n' + 'JOIN_ID: ' + str(JOIN_ID)
+                + '\n' + 'CLIENT_NAME: ' + str(CLIENT_NAME) + '\n')
+                server_connection.sendall(msg)
+			
+            elif '<disconnect>' in msg:
+                msg = ('DISCONNECT: ' + str(CLIENT_IP) + '\n' + 'PORT: ' 
+                + str(PORT) + '\n' + 'CLIENT_NAME: ' + str(CLIENT_NAME) + '\n')
+                server_connection.sendall(msg)
+			
+            else:
+                data = 'CHAT: ' + str(ROOM_REF) + '\n' + 'CLIENT_NAME: ' + str(CLIENT_NAME) + '\n' + 'MESSAGE: ' + str(msg) + '\n' 
+                server_connection.sendall(data)
+
+		    
+                
+            
+
+            
+            
+
+
+		
 		
